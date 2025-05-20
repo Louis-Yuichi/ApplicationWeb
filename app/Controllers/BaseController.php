8a65708178938f9090560ca6371a77777efdcc0b
ThaoDujardin <thaodujardin@gmail.com>
2025-05-20 15:31:02 +0200

    Implement Twig integration and update view rendering in BaseController; add template.html.twig

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -9,6 +9,8 @@ use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
+require_once __DIR__ . '/../../vendor/autoload.php';
+
 /**
  * Class BaseController
  *
@@ -21,38 +23,55 @@ use Psr\Log\LoggerInterface;
  */
 abstract class BaseController extends Controller
 {
-    /**
-     * Instance of the main Request object.
-     *
-     * @var CLIRequest|IncomingRequest
-     */
-    protected $request;
+	/**
+	 * Instance of the main Request object.
+	 *
+	 * @var CLIRequest|IncomingRequest
+	 */
+	protected $request;
 
-    /**
-     * An array of helpers to be loaded automatically upon
-     * class instantiation. These helpers will be available
-     * to all other controllers that extend BaseController.
-     *
-     * @var list<string>
-     */
-    protected $helpers = [];
+	/**
+	 * An array of helpers to be loaded automatically upon
+	 * class instantiation. These helpers will be available
+	 * to all other controllers that extend BaseController.
+	 *
+	 * @var list<string>
+	 */
+	protected $helpers = [];
 
-    /**
-     * Be sure to declare properties for any property fetch you initialized.
-     * The creation of dynamic property is deprecated in PHP 8.2.
-     */
-    // protected $session;
+	/**
+	 * Be sure to declare properties for any property fetch you initialized.
+	 * The creation of dynamic property is deprecated in PHP 8.2.
+	 */
+	// protected $session;
 
-    /**
-     * @return void
-     */
-    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
-    {
-        // Do Not Edit This Line
-        parent::initController($request, $response, $logger);
+	/**
+	 * @return void
+	 */
+	public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+	{
+		// Do Not Edit This Line
+		parent::initController($request, $response, $logger);
 
-        // Preload any models, libraries, etc, here.
+		// Preload any models, libraries, etc, here.
 
-        // E.g.: $this->session = service('session');
-    }
+		// E.g.: $this->session = service('session');
+	}
+
+	protected function view(string $viewName, array $data = []) {
+
+		$viewsPath = realpath(__DIR__ . '/../Views');
+		$loader = new \Twig\Loader\FilesystemLoader($viewsPath);
+		$twig = new \Twig\Environment($loader, [
+			'cache' => false, // Mettre un dossier ('cache/') en production pour améliorer les performances
+		]);
+
+// Rendu du template accueil.twig avec des variables
+		echo $twig->render($viewName,$data);
+	}
+}
+
+// Test Twig autoload
+if (!class_exists('\Twig\Environment')) {
+    die('Twig n\'est pas chargé !');
 }
