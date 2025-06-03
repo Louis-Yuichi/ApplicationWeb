c1eaba286f2acf3c63be9a23fa7dd38bccd17cfb
Thomazeau-Agullo Louis <louisagullo.05@gmail.com>
2025-06-03 15:41:42 +0200

    maj

--- app/Database/Migrations/2025-06-03-094740_CreateUtilisateur.php
+++ app/Database/Migrations/2025-06-03-094740_CreateUtilisateur.php
@@ -8,32 +8,21 @@ class CreateUtilisateur extends Migration
 {
 	public function up()
 	{
-		$this->forge->addField([
-			'numCandidat'         => ['type' => 'VARCHAR', 'constraint' => 20],
-			'anneeUniversitaire'  => ['type' => 'VARCHAR', 'constraint' => 10],
-			'nom'                 => ['type' => 'VARCHAR', 'constraint' => 255],
-			'prenom'              => ['type' => 'VARCHAR', 'constraint' => 255],
-			'profil'              => ['type' => 'TEXT', 'null' => true],
-			'groupe'              => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
-			'marqueurDossier'     => ['type' => 'TEXT', 'null' => true],
-			'scolarite'           => ['type' => 'TEXT', 'null' => true],
-			'diplome'             => ['type' => 'TEXT', 'null' => true],
-			'preparation_obtenu'  => ['type' => 'TEXT', 'null' => true],
-			'serie'               => ['type' => 'TEXT', 'null' => true],
-			'specialitesTerminale'=> ['type' => 'TEXT', 'null' => true],
-			'specialiteAbandonne' => ['type' => 'TEXT', 'null' => true],
-			'noteLycee'           => ['type' => 'DECIMAL', 'constraint' => '5,2', 'null' => true],
-			'noteFicheAvenir'     => ['type' => 'DECIMAL', 'constraint' => '5,2', 'null' => true],
-			'noteDossier'         => ['type' => 'DECIMAL', 'constraint' => '5,2', 'null' => true],
-			'commentaire'         => ['type' => 'TEXT', 'null' => true]
+		$this->forge->addField
+		([
+			'idUtilisateur'     => ['type' => 'INT'    , 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+			'nomUtilisateur'    => ['type' => 'VARCHAR', 'constraint' => 50],
+			'prenomUtilisateur' => ['type' => 'VARCHAR', 'constraint' => 50],
+			'mailUtilisateur'   => ['type' => 'VARCHAR', 'constraint' => 78],
+			'mdpUtilisateur'    => ['type' => 'VARCHAR', 'constraint' => 64]
 		]);
-		
-		$this->forge->addPrimaryKey('numCandidat');
-		$this->forge->createTable('candidat', true);
+
+		$this->forge->addPrimaryKey('idUtilisateur');
+		$this->forge->createTable('Utilisateur', true);
 	}
 
 	public function down()
 	{
-		$this->forge->dropTable('candidat');
+		$this->forge->dropTable('Utilisateur');
 	}
 }
