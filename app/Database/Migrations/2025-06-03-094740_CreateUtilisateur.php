7b6220549a373d196fc1d30e103a9a4f9eb59fb3
Thomazeau-Agullo Louis <louisagullo.05@gmail.com>
2025-06-03 11:57:42 +0200

    THAO

--- app/Database/Migrations/2025-06-03-094740_CreateUtilisateur.php
+++ app/Database/Migrations/2025-06-03-094740_CreateUtilisateur.php
@@ -4,7 +4,7 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class CreateCandidatTable extends Migration
+class CreateUtilisateur extends Migration
 {
 	public function up()
 	{
