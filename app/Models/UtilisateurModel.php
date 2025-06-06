c5f25c95b1281acd83ba9e631b829978de96d1a5
Thomazeau-Agullo Louis <louisagullo.05@gmail.com>
2025-06-06 12:06:45 +0200

    maj

--- app/Models/UtilisateurModel.php
+++ app/Models/UtilisateurModel.php
@@ -6,16 +6,17 @@ use CodeIgniter\Model;
 
 class UtilisateurModel extends Model
 {
-    protected $table = 'UtilisateurWeb';
-    protected $primaryKey = 'idUtilisateur';
-    protected $allowedFields = ['nomUtilisateur', 'prenomUtilisateur', 'mailUtilisateur', 'mdpUtilisateur'];
-    protected $useAutoIncrement = true;
-    protected $returnType = 'array';
+	protected $table            = 'UtilisateurWeb';
+	protected $primaryKey       = 'idUtilisateur';
+	protected $allowedFields    = ['nomUtilisateur', 'prenomUtilisateur', 'mailUtilisateur', 'mdpUtilisateur'];
+	protected $useAutoIncrement = true;
+	protected $returnType       = 'array';
 
-    protected $validationRules = [
-        'nomUtilisateur' => 'required|min_length[2]|max_length[50]',
-        'prenomUtilisateur' => 'required|min_length[2]|max_length[50]',
-        'mailUtilisateur' => 'required|valid_email|is_unique[UtilisateurWeb.mailUtilisateur]',
-        'mdpUtilisateur' => 'required|min_length[6]'
-    ];
+	protected $validationRules =
+	[
+		'nomUtilisateur'    => 'required|min_length[2]|max_length[50]',
+		'prenomUtilisateur' => 'required|min_length[2]|max_length[50]',
+		'mailUtilisateur'   => 'required|valid_email|is_unique[UtilisateurWeb.mailUtilisateur]',
+		'mdpUtilisateur'    => 'required|min_length[6]'
+	];
 }
