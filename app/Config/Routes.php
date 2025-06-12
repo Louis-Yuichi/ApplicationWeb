c4aff6ef1bb022c7fba88b4559b0252ad6f7fc05
Thomazeau-Agullo Louis <louisagullo.05@gmail.com>
2025-06-12 15:15:32 +0200

    add route importe scodoc

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,17 +5,18 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->post('login', 'Home::login');
-$routes->get('register', 'Home::register');
-$routes->match(['GET', 'POST'], 'register', 'Home::register');
-$routes->get('logout', 'Home::logout');
-$routes->get('accueil', 'Home::accueil');
+$routes->get  ('/'       , 'Home::index'   );
+$routes->post ('login'   , 'Home::login'   );
+$routes->get  ('register', 'Home::register');
+$routes->match(['GET'    , 'POST'], 'register', 'Home::register');
+$routes->get  ('logout'  , 'Home::logout'  );
+$routes->get  ('accueil' , 'Home::accueil' );
 
 // ParcourSupController routes
-$routes->get('parcoursup', 'ParcourSupController::menu');
-$routes->get('gestionParcourSup', 'ParcourSupController::gestion');
-$routes->post('importer', 'ParcourSupController::importer');
+$routes->get ('parcoursup'       , 'ParcourSupController::menu'    );
+$routes->get ('gestionParcourSup', 'ParcourSupController::gestion' );
+$routes->post('importer'         , 'ParcourSupController::importer');
 
 // ScodocController routes
-$routes->get('scodoc', 'ScodocController::menu');
+$routes->get ('scodoc'  , 'ScodocController::menu'    );
+$routes->post('importer', 'ScodocController::importer');
